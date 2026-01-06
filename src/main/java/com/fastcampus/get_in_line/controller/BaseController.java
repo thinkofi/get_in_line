2f722695882e0764062371e479302b57369a758e
Seongho Ahn <thinkofi@hanmail.net>
2026-01-06 19:12:41 +0900

    Initalize project source

---
+++ src/main/java/com/fastcampus/get_in_line/controller/BaseController.java
@@ -0,0 +1,19 @@
+package com.fastcampus.get_in_line.controller;
+
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController implements ErrorController {
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
+    }
+}
---
+++ src/test/java/com/fastcampus/get_in_line/GetInLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.get_in_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
